954c792a890aef25e8a2cdf8a5b9fddb1ef41db0
Han K Ngo <100813911+hankngo@users.noreply.github.com>
2024-05-02 19:34:54 -0700

    Setting up React for project

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles/App.css'; // Adjust the path if necessary
+import Header from './components/layout/Header';
+import logo from './assets/logo.svg';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* Other components will be added here */}
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/layout/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../styles/Header.css'; // This will be your CSS file
+
+const Header = () => (
+  <header className="header">
+    <h1>Welcome to My Website</h1>
+  </header>
+);
+
+export default Header;
