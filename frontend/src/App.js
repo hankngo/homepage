c8124de8ec73856b4d8b55385b65eae225903fb9
Han K Ngo <100813911+hankngo@users.noreply.github.com>
2024-05-05 22:43:02 -0700

    Add SeachSection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './styles/App.css'; 
 import logo from './assets/logo.svg';
 import Navbar from './components/layout/Navbar';
+import SearchSection from './components/layout/SearchSection';
 
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <SearchSection />
       {/* Other components will be added here */}
     </div>
   );
---
+++ frontend/src/components/layout/SearchSection.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../../styles/SearchSection.css'; // Make sure this is your CSS file path
+
+const SearchSection = () => {
+  return (
+    <div className="search-section">
+      <div className="welcome-banner">
+        Welcome to our future website! <a href="#learn-more">Learn more</a>
+      </div>
+      <img src="https://storage.googleapis.com/proudcity/williamstonmi/uploads/2024/01/IMG_1404_copy-min-2000x1333.jpeg" alt="Beautiful Williamston, MI" className="search-image" />
+      <div className="search-content">
+        <h1>Proudly serving Williamston, MI</h1>
+        <p>How can we help you?</p>
+        <div className="search-box">
+          <input type="text" placeholder="Search..." />
+          <button type="submit"><i className="fas fa-search"></i></button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchSection;
